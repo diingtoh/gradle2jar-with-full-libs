6e13bdcde55d2b90070c9d547ca00372c05cc152
diingtoh <zscpro@yahoo.com>
2021-03-23 19:02:27 +0800

    test git update

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -2,8 +2,7 @@ package hello;
 
 import  org.joda.time.LocalTime;
 
-
-
+//test git
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
